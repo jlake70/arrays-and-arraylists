ee9f03703ebcffc489779a290f68c073b6c7d6c9
Joshua <jlake7021@gmail.com>
2023-06-08 13:31:23 -0400

    arrays practice

--- src/arrays_and_arraylists/ArraysPractice.java
+++ src/arrays_and_arraylists/ArraysPractice.java
@@ -1,9 +1,21 @@
 package arrays_and_arraylists;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
+
+
     public static void main(String[] args) {
 //        Create an empty array with 5 elements
+        int[] myArray = new int[5];
+        myArray[0] = 6;
+        myArray[1] = 5;
+        myArray[2] = 7;
+        myArray[3] = 9;
+        myArray[4] = 8;
+        int[] meArray = {12,13,14,15,8};
 //        give each index in the array a value
+
 //        print the entire array to the terminal (you will need to use the Array.toString() method)
 //        print the length of the array
 
@@ -13,6 +25,11 @@ public class ArraysPractice {
 
 //        change a few values in each array and then print both arrays to the terminal again
         System.out.println("Hello World");
+        System.out.println(Arrays.toString(myArray));
+        System.out.println(myArray.length);
+        System.out.println(Arrays.toString(meArray));
+        System.out.println(meArray.length);
+
     }
 
 
